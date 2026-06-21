765b31a2f4ab94d273f22fb08253c371f4e195b1
IanDude <iandudley012@gmail.com>
2026-06-21 21:13:58 +0800

    refactor(auth): Converted auth controller endpoints to use services, added email service for email notification functions

--- src/controllers/v1/auth.controllers.js
+++ src/controllers/v1/auth.controllers.js
@@ -3,13 +3,17 @@ import nodemailer from "nodemailer";
 
 import { catchAsync } from "../../util/catchAsync.js";
 import { validateBody } from "../../util/validation.js";
-import { loginSchema, registerSchema } from "../../schemas/auth.schema.js";
+import { forgotPasswordSchema, loginSchema, registerSchema } from "../../schemas/auth.schema.js";
 import { comparePassword, hashPassword } from "../../util/passwordHelpers.js";
 import { generateToken, generatePasswordToken, hashPasswordToken } from "../../util/tokenHelpers.js";
 import { loginLimiter, registerLimiter } from "../../middlewares/rateLimiters.js";
 import { ERROR_CODES } from "../../util/APIError.js";
 import authenticate from "../../middlewares/authenticate.js";
 import generateUUID from "../../util/generateUUID.js";
+import * as authService from "../../services/authService.js";
+
+//Test imports
+import * as userRepository from "../../repository/userRepository.js";
 
 const router = Router();
 
@@ -28,43 +32,9 @@ router.post(
   loginLimiter,
   validateBody(loginSchema),
   catchAsync(async (req, res) => {
-    const { email, password } = req.body;
+    const result = await authService.login(req.body, req.db);
 
-    const [user] = await req.db.query(
-      `
-      SELECT id, user_uuid, email, password, salt, status
-      FROM users WHERE email = ?
-      `,
-      [email],
-    );
-
-    if (!user) {
-      return res.sendError("No user found", "Account not found", 404, ERROR_CODES.USER_NOT_FOUND);
-    }
-
-    const isPasswordValid = await comparePassword(password, user.password, user.salt);
-
-    if (!isPasswordValid) {
-      return res.sendError("Incorrect Password", "Invalid Credentials", 401, ERROR_CODES.INVALID_CREDENTIALS);
-    }
-
-    const rows = await req.db.query(
-      `
-      SELECT r.name FROM user_roles ur
-      INNER JOIN roles r ON ur.role_id = r.id
-      WHERE ur.user_id = ?;`,
-      [user.id],
-    );
-
-    const roles = rows.map((role) => role.name);
-
-    const token = generateToken({ user_uuid: user.user_uuid, roles: roles });
-
-    res.sendSuccess(
-      "Logged In Successfully",
-      { user: { user_uuid: user.user_uuid, email: user.email, status: user.status }, token },
-      200,
-    );
+    res.sendSuccess("Logged In Successfully", result, 200);
   }),
 );
 
@@ -74,31 +44,9 @@ router.post(
   registerLimiter,
   validateBody(registerSchema),
   catchAsync(async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
+    const result = await authService.register(req.body, req.db);
 
-    const [existing] = await req.db.query("SELECT email FROM users WHERE email = ?", [email]);
-
-    if (existing && (existing.length !== 0 || existing.email)) {
-      res.sendError(
-        "Email is already taken, use a different one",
-        "Email already exists",
-        409,
-        ERROR_CODES.DUPLICATE_ENTRY,
-      );
-    }
-
-    const { hashedPassword, salt } = await hashPassword(password);
-
-    await req.db.query("INSERT INTO users SET ?", {
-      user_uuid: generateUUID(),
-      first_name: firstName,
-      last_name: lastName,
-      email,
-      password: hashedPassword,
-      salt: salt,
-      status: "active",
-    });
-    res.sendSuccess("Registered Successfully!", 201);
+    res.sendSuccess("Registered Successfully!", result, 201);
   }),
 );
 
@@ -120,116 +68,121 @@ router.get(
 //POST /forgot-password - Endpoint for password recovery
 router.post(
   "/forgot-password",
+  //Add a limiter on how often this endpoint can be called
+  validateBody(forgotPasswordSchema),
   catchAsync(async (req, res) => {
     const { email } = req.body;
 
-    const [row] = await req.db.query("SELECT user_uuid FROM users WHERE email = ?", [email]);
-    if (!row) return res.sendError("If this email exists, a reset link has been sent.");
+    await authService.forgotPassword(req.body, req.db);
 
-    const rawToken = generatePasswordToken();
-    const hashedToken = hashPasswordToken(rawToken);
-    const user_uuid = row.user_uuid;
+    res.sendSuccess("If this email exists, a reset link has been sent.");
+    // const [row] = await req.db.query("SELECT user_uuid FROM users WHERE email = ?", [email]);
+    // if (!row) return res.sendError("If this email exists, a reset link has been sent.");
 
-    //Query to save hashed token to db goes here
+    // const rawToken = generatePasswordToken();
+    // const hashedToken = hashPasswordToken(rawToken);
+    // const user_uuid = row.user_uuid;
 
-    const saveToken = await req.db.query(
-      "INSERT INTO password_tokens (user_uuid, token_hash, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 1 HOUR))",
-      [user_uuid, hashedToken],
-    );
+    // //Query to save hashed token to db goes here
 
-    if (saveToken.affectedRows === 0) return res.sendError("Failed to insert reset token");
+    // const saveToken = await req.db.query(
+    //   "INSERT INTO password_tokens (user_uuid, token_hash, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 1 HOUR))",
+    //   [user_uuid, hashedToken],
+    // );
 
-    const resetLink = `${process.env.FRONTEND_URL}/reset-password.html?token=${rawToken}`;
+    // if (saveToken.affectedRows === 0) return res.sendError("Failed to insert reset token");
 
-    //Send email using nodemailer
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASS,
-      },
-    });
+    // const resetLink = `${process.env.FRONTEND_URL}/reset-password.html?token=${rawToken}`;
 
-    const template = `
-    <!DOCTYPE html>
-    <html>
-      <body style="margin:0; padding:0; font-family: Arial, sans-serif; background-color:#f4f6f8;">
-        <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#f4f6f8; padding:20px;">
-          <tr>
-            <td align="center">
-            <!-- Card -->
-            <table width="400" cellpadding="0" cellspacing="0" style="background:#ffffff; padding:24px; border-radius:8px;">
-        
-          <tr>
-            <td>
-              <h2 style="margin:0 0 10px;">Reset Your Password</h2>
-              <p style="font-size:14px; color:#555;">
-                You requested to reset your password. Click the button below to continue.
-              </p>
-            </td>
-          </tr>
+    // //Send email using nodemailer
+    // let transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.GMAIL_USER,
+    //     pass: process.env.GMAIL_APP_PASS,
+    //   },
+    // });
 
-          <!-- Button -->
-          <tr>
-            <td align="center" style="padding:20px 0;">
-              <a href="{{RESET_LINK}}" 
-                style="
-                  display:inline-block;
-                  padding:12px 20px;
-                  background-color:#111;
-                  color:#ffffff;
-                  text-decoration:none;
-                  border-radius:6px;
-                  font-size:14px;
-                ">
-                Reset Password
-              </a>
-            </td>
-          </tr>
+    // const template = `
+    // <!DOCTYPE html>
+    // <html>
+    //   <body style="margin:0; padding:0; font-family: Arial, sans-serif; background-color:#f4f6f8;">
+    //     <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#f4f6f8; padding:20px;">
+    //       <tr>
+    //         <td align="center">
+    //         <!-- Card -->
+    //         <table width="400" cellpadding="0" cellspacing="0" style="background:#ffffff; padding:24px; border-radius:8px;">
 
-          <!-- Fallback link -->
-          <tr>
-            <td>
-              <p style="font-size:12px; color:#888;">
-                If the button doesn’t work, copy and paste this link into your browser:
-              </p>
-              <p style="font-size:12px; word-break:break-all; color:#555;">
-                {{RESET_LINK}}
-              </p>
-            </td>
-          </tr>
+    //       <tr>
+    //         <td>
+    //           <h2 style="margin:0 0 10px;">Reset Your Password</h2>
+    //           <p style="font-size:14px; color:#555;">
+    //             You requested to reset your password. Click the button below to continue.
+    //           </p>
+    //         </td>
+    //       </tr>
 
-          <!-- Footer -->
-          <tr>
-            <td style="padding-top:20px;">
-              <p style="font-size:12px; color:#999;">
-                If you did not request this, you can safely ignore this email.
-              </p>
-            </td>
-          </tr>
-        </table>
-        </td>
-        </tr>
-      </table>
-      </body>
-    </html>
+    //       <!-- Button -->
+    //       <tr>
+    //         <td align="center" style="padding:20px 0;">
+    //           <a href="{{RESET_LINK}}"
+    //             style="
+    //               display:inline-block;
+    //               padding:12px 20px;
+    //               background-color:#111;
+    //               color:#ffffff;
+    //               text-decoration:none;
+    //               border-radius:6px;
+    //               font-size:14px;
+    //             ">
+    //             Reset Password
+    //           </a>
+    //         </td>
+    //       </tr>
 
-    `;
+    //       <!-- Fallback link -->
+    //       <tr>
+    //         <td>
+    //           <p style="font-size:12px; color:#888;">
+    //             If the button doesn’t work, copy and paste this link into your browser:
+    //           </p>
+    //           <p style="font-size:12px; word-break:break-all; color:#555;">
+    //             {{RESET_LINK}}
+    //           </p>
+    //         </td>
+    //       </tr>
 
-    let message = {
-      from: `"Support" <${process.env.GMAIL_USER}>`,
-      to: email,
-      subject: "Test Email",
-      html: template.replace(/{{RESET_LINK}}/g, resetLink),
-    };
+    //       <!-- Footer -->
+    //       <tr>
+    //         <td style="padding-top:20px;">
+    //           <p style="font-size:12px; color:#999;">
+    //             If you did not request this, you can safely ignore this email.
+    //           </p>
+    //         </td>
+    //       </tr>
+    //     </table>
+    //     </td>
+    //     </tr>
+    //   </table>
+    //   </body>
+    // </html>
 
-    try {
-      const info = await transporter.sendMail(message);
-      // console.log(info);
-      res.sendSuccess("If this email exists, a reset link has been sent.");
-    } catch (error) {
-      res.sendError("Failed to send email", error);
-    }
+    // `;
+
+    // let message = {
+    //   from: `"Support" <${process.env.GMAIL_USER}>`,
+    //   to: email,
+    //   subject: "Test Email",
+    //   html: template.replace(/{{RESET_LINK}}/g, resetLink),
+    // };
+
+    // try {
+    //   const info = await transporter.sendMail(message);
+    //   console.log(info.response, info.response.includes("OK"));
+    //   res.sendSuccess("If this email exists, a reset link has been sent.");
+    // } catch (error) {
+    //   res.sendError("Failed to send email", error);
+    // }
   }),
 );
 
@@ -237,37 +190,39 @@ router.post(
 router.post(
   "/reset-password",
   catchAsync(async (req, res) => {
-    const { token, newPassword } = req.body;
+    // const { token, newPassword } = req.body;
 
-    const hashedToken = hashPasswordToken(token);
+    await authService.resetPassword(req.body, req.db);
 
-    const [passwordToken] = await req.db.query(
-      "SELECT * FROM password_tokens WHERE token_hash = ? AND expires_at > NOW() AND used_at IS NULL",
-      [hashedToken],
-    );
+    // const hashedToken = hashPasswordToken(token);
 
-    // console.log(passwordToken);
+    // const [passwordToken] = await req.db.query(
+    //   "SELECT * FROM password_tokens WHERE token_hash = ? AND expires_at > NOW() AND used_at IS NULL",
+    //   [hashedToken],
+    // );
 
-    if (passwordToken.length === 0) return res.sendError("Invalid or expired token");
+    // // console.log(passwordToken);
 
-    const hashedPassword = await hashPassword(newPassword);
+    // if (passwordToken.length === 0) return res.sendError("Invalid or expired token");
 
-    const updatePassword = await req.db.query("UPDATE users SET password = ? WHERE user_uuid = ?", [
-      hashedPassword,
-      passwordToken.user_uuid,
-    ]);
+    // const hashedPassword = await hashPassword(newPassword);
 
-    if (updatePassword.affectedRows === 0) return res.sendError("Failed to update password");
+    // const updatePassword = await req.db.query("UPDATE users SET password = ? WHERE user_uuid = ?", [
+    //   hashedPassword,
+    //   passwordToken.user_uuid,
+    // ]);
 
-    //Invalidate password token to prevent re-use
-    const updatePasswordToken = await req.db.query(
-      "UPDATE password_tokens SET used_at = NOW() WHERE id = ?",
-      passwordToken.id,
-    );
+    // if (updatePassword.affectedRows === 0) return res.sendError("Failed to update password");
 
-    if (updatePasswordToken.affectedRows === 0) return req.sendError("Failed to invalidate password token");
+    // //Invalidate password token to prevent re-use
+    // const updatePasswordToken = await req.db.query(
+    //   "UPDATE password_tokens SET used_at = NOW() WHERE id = ?",
+    //   passwordToken.id,
+    // );
 
-    console.log(passwordToken);
+    // if (updatePasswordToken.affectedRows === 0) return req.sendError("Failed to invalidate password token");
+
+    // // console.log(passwordToken);
 
     res.sendSuccess("Password reset successful");
   }),
---
+++ src/services/authService.js
@@ -0,0 +1,106 @@
+import * as userRepository from "../repository/userRepository.js";
+import * as roleRepository from "../repository/roleRepository.js";
+import APIError, { ERROR_CODES } from "../util/APIError.js";
+import { comparePassword, hashPassword } from "../util/passwordHelpers.js";
+import { generatePasswordToken, generateToken, hashPasswordToken } from "../util/tokenHelpers.js";
+import generateUUID from "../util/generateUUID.js";
+import * as passwordRepository from "../repository/passwordRepository.js";
+import * as emailService from "./emailService.js";
+
+export async function login({ email, password }, db) {
+  const user = await userRepository.findByEmail(email, db);
+
+  if (!user) {
+    // throw { message: "Account not found", status: 404, code: ERROR_CODES.USER_NOT_FOUND };
+    throw new APIError("Account not found", 404, ERROR_CODES.USER_NOT_FOUND);
+  }
+
+  const isValid = await comparePassword(password, user.password, user.salt);
+
+  if (!isValid) {
+    throw new APIError("Invalid email or password", 401, ERROR_CODES.INVALID_CREDENTIALS);
+  }
+
+  const roles = await roleRepository.findById(user.id, db);
+
+  const token = generateToken({ user_uuid: user.user_uuid, roles });
+
+  return { user: { user_uuid: user.user_uuid, email: user.email, status: user.status }, token };
+}
+
+export async function register({ firstName, lastName, email, password }, db) {
+  const isExisting = await userRepository.findByEmail(email, db);
+
+  if (isExisting) {
+    throw new APIError("Email is taken, use a diffferent one", 409, ERROR_CODES.DUPLICATE_ENTRY);
+  }
+
+  const { hashedPassword, salt } = await hashPassword(password);
+
+  const userData = {
+    user_uuid: generateUUID(),
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    password: hashedPassword,
+    salt,
+    status: "active",
+  };
+
+  const createNewUser = await userRepository.createNewUser(userData, db);
+  if (createNewUser.affectedRows === 0) {
+    throw new APIError("Failed to create new user", 500, ERROR_CODES.DATABASE_ERROR);
+  }
+
+  return { user_uuid: userData.user_uuid, email, status: userData.status };
+}
+
+export async function forgotPassword({ email }, db) {
+  const user = await userRepository.findByEmail(email, db);
+
+  if (!user) {
+    throw new APIError("If this email exists, a reset link has been sent", 400, ERROR_CODES.INVALID_INPUT);
+  }
+
+  const rawToken = generatePasswordToken();
+  const hashedToken = hashPasswordToken(rawToken);
+
+  const insertToken = await passwordRepository.savePasswordToken(user.user_uuid, hashedToken, db);
+
+  if (insertToken.affectedRows === 0) {
+    throw new APIError("Failed to save password token");
+  }
+
+  const resetLink = `${process.env.FRONTEND_URL}/reset-password.html?token=${rawToken}`;
+
+  try {
+    await emailService.sendPasswordResetEmail(email, resetLink);
+  } catch (error) {
+    throw new APIError("Failed to send email", 500, ERROR_CODES.INTERNAL_ERROR);
+  }
+}
+
+export async function resetPassword({ token, newPassword }, db) {
+  const hashedToken = hashPasswordToken(token);
+
+  const passwordToken = await passwordRepository.findValidToken(hashedToken, db);
+
+  if (!passwordToken) {
+    throw new APIError("Invalid or expired token", 400, ERROR_CODES.INVALID_ORIGIN);
+  }
+
+  const user_uuid = passwordToken.user_uuid;
+  const { hashedPassword } = await hashPassword(newPassword);
+
+  const updatePassword = await userRepository.updateUserPassword({ user_uuid, hashedPassword }, db);
+
+  if (updatePassword.affectedRows === 0) {
+    throw new APIError("Failed to update password", 500, ERROR_CODES.DATABASE_ERROR);
+  }
+
+  const invalidateAllPasswordToken = await passwordRepository.invalidateAllPasswordToken(user_uuid, db);
+
+  if (invalidateAllPasswordToken.affectedRows === 0) {
+    throw new APIError("Failed to invalidate password token", 400, ERROR_CODES.DATABASE_ERROR);
+  }
+}
